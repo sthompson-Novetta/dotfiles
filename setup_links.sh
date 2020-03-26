64a033d85ad6f86e121793cb7b2d469577138803
Scott M. Thompson <scott.thompson@dodiis.mil>
2020-03-26 14:36:01 -0400

    Update to setup links

--- setup_links.sh
+++ setup_links.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+[[ -L $HOME/.gitignore ]] && rm $HOME/.gitignore
+[[ -f $HOME/.gitignore ]] && mv $HOME/.gitignore $HOME/gitignore.bak
 ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
+
+[[ -L $HOME/.bashrc ]] && rm $HOME/.bashrc
+[[ -f $HOME/.bashrc ]] && mv $HOME/.bashrc $HOME/bashrc.bak
 ln -s $HOME/dotfiles/bashrc $HOME/.bashrc
+
+[[ -L $HOME/.tmux.conf ]] && rm $HOME/.tmux.conf
+[[ -f $HOME/.tmux.conf ]] && mv $HOME/.tmux.conf $HOME/tmux.conf.bak
 ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 
 
